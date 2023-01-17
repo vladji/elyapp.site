ed1bf12068c3b6e876957bcbaf4bb71dddb2e72f
vladji <vladjislav@gmail.com>
2023-01-17 15:39:13 +0600

    feat: product section

--- src/components/ProductBlock/index.tsx
+++ src/components/ProductBlock/index.tsx
@@ -8,30 +8,101 @@ export const ProductBlock: FC = () => {
     <section>
       <Card>
         <h2 className={styles.title}>{useLang('product.title')}</h2>
-        <p className={styles.description}>{useLang('product.description')}</p>
-        <h3 className={styles.subtitle}>{useLang('product.subtitle')}</h3>
-        <ul className={styles.bulletsWrapper}>
-          <li className={styles.bullet}>
-            <span>-&nbsp;</span>
-            <span>{useLang('product.bul1')}</span>
-          </li>
-          <li className={styles.bullet}>
-            <span>-&nbsp;</span>
-            <span>{useLang('product.bul2')}</span>
-          </li>
-          <li className={styles.bullet}>
-            <span>-&nbsp;</span>
-            <span>{useLang('product.bul3')}</span>
-          </li>
-          <li className={styles.bullet}>
-            <span>-&nbsp;</span>
-            <span>{useLang('product.bul4')}</span>
-          </li>
-        </ul>
-        {/*<div className={styles.priceWrapper}>*/}
-        {/*  <span className={styles.priceValue}>200 000</span>*/}
-        {/*  <span className={styles.priceSign}>&nbsp;{useLang('tenge')}</span>*/}
-        {/*</div>*/}
+        <div className={styles.item}>
+          <h3 className={styles.itemTitle}>{useLang('product.var1.title')}</h3>
+          <ul>
+            <li>
+              <div className={styles.itemDetail}>
+                <span>-&nbsp;</span>
+                <span>
+                    работа с МАК (метафоричкские ассоциативеные карты) + коуч сессия для глубинной проработки и решения проблемы лишнего веса в корне
+                </span>
+              </div>
+            </li>
+          </ul>
+          <div className={styles.itemConditions}>
+            <p className={styles.itemPeriod}>{useLang('product.var1.period')}</p>
+            <p className={styles.itemPrice}>{useLang('product.var1.price')}</p>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <h3 className={styles.itemTitle}>{useLang('product.var2')}</h3>
+          <ul>
+            <li>
+              <div className={styles.itemDetail}>
+                <span>-&nbsp;</span>
+                <span>
+                    проработка пищевого поведения и лишнего веса
+                  </span>
+              </div>
+            </li>
+            <li>
+              <div className={styles.itemDetail}>
+                <span>-&nbsp;</span>
+                <span>
+                    работа по диетологии (формирование здорового меню, рецепты, продуктоведение)
+                  </span>
+              </div>
+            </li>
+            <li>
+              <div className={styles.itemDetail}>
+                <span>-&nbsp;</span>
+                <span>
+                    рекомендации по физической активности, уходу за кожей, анти стресс и в целом по зож
+                  </span>
+              </div>
+            </li>
+            <li>
+              <div className={styles.itemDetail}>
+                <span>-&nbsp;</span>
+                <span>
+                    сопровождение процесса похудения, контроль результатов
+                </span>
+              </div>
+            </li>
+          </ul>
+          <div className={styles.itemConditions}>
+            <p className={styles.itemPeriod}>{useLang('product.var2.v1.period')}</p>
+            <p className={styles.itemPrice}>{useLang('product.var2.v1.price')}</p>
+          </div>
+          <div className={styles.itemConditions}>
+            <p className={styles.itemPeriod}>{useLang('product.var2.v2.period')}</p>
+            <p className={styles.itemPrice}>{useLang('product.var2.v2.price')}</p>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <h3 className={styles.itemTitle}>{useLang('product.var3')}</h3>
+          <ul>
+            <li>
+              <div className={styles.itemDetail}>
+                <span>-&nbsp;</span>
+                <span>{useLang('product.bul1')}</span>
+              </div>
+            </li>
+            <li>
+              <div className={styles.itemDetail}>
+                <span>-&nbsp;</span>
+                <span>{useLang('product.bul2')}</span>
+              </div>
+            </li>
+            <li>
+              <div className={styles.itemDetail}>
+                <span>-&nbsp;</span>
+                <span>{useLang('product.bul3')}</span>
+              </div>
+            </li>
+            <li>
+              <div className={styles.itemDetail}>
+                <span>-&nbsp;</span>
+                <span>{useLang('product.bul4')}</span>
+              </div>
+            </li>
+          </ul>
+          <div className={styles.itemConditions}>
+            <p className={styles.itemPeriod}>{useLang('product.var3.period')}</p>
+            <p className={styles.itemPrice}>{useLang('product.var3.price')}</p>
+          </div>
+        </div>
       </Card>
     </section>
   );
